b67e76eafd5d58e2c592c203c6801f5f45a71162
Himanshu Dongre <ymrx5094@gmail.com>
2023-11-14 19:01:40 -0600

    requests attempt

--- proxy.py
+++ proxy.py
@@ -1,7 +1,4 @@
-from mitmproxy import io, http, proxy, options, addons, tools
-from mitmproxy.tools import main
-from mitmproxy.tools.dump import DumpMaster
-import asyncio
+import requests
 import argparse
 
 
@@ -13,24 +10,8 @@ def main():
     argparser.add_argument("listening_port", help="The port your proxy will listen for connections on", type=int)
     args = argparser.parse_args()
 
-    opts = options.Options(listen_host=args.listening_ip, listen_port=args.listening_port)
-    m = DumpMaster(options=opts)
-    m.server = addons.proxyserver.ProxyServer()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(m.run())
-
-
-
-def request(flow: http.HTTPFlow) -> None:
-    print("[*] Received request:")
-    print(flow.request.headers)
-    print(flow.request.text)
-
-
-def response(flow: http.HTTPFlow) -> None:
-    print("[*] Received response:")
-    print(flow.response.headers)
-    print(flow.response.text)
+    proxy = {"http": f"http://{args.listening_ip}:{args.listening_port}"}
+    r = requests.get('http://www.google.com/', proxies=proxy)
 
 
 if __name__ == "__main__":
